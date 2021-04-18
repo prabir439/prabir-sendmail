facd6fe517842fef007f59851c94832a57801279
Prabir chowdhury <prabirchy439@gmail.com>
2021-04-18 19:59:45 +0600

    add readme file :

--- src/views/contact.blade.php
+++ src/views/contact.blade.php
@@ -1,20 +1,30 @@
 <html>
     <head>
-        <title> Contact Form</title>
+        <title> Send Mail </title>
+        <style>
+            .content {
+                margin: auto;
+                width: 60%;
+                padding: 10px;
+            }
+        </style>
     </head>
     <body>
-        <form action="{{route('contact')}}" method="POST">
-            @csrf
-            <label> Name </label><br>
-            <input type="text" name="name"   placeholder="Name"> <br>
-            <label> Email </label><br>
-
-            <input type="email" name="email" placeholder="Email"> <br>
-            <label> Message </label><br>
-
-            <textarea type="text" name="message"></textarea><br>
-            <button type="submit"> Send Email</button>
-        </form>
+    <div class="content">
+        <fieldset>
+            <legend>Send Email :</legend>
+            <form action="{{route('contact')}}" method="POST">
+                @csrf
+                <label> Your Name </label><br>
+                <input type="text" name="name"   placeholder="Name"> <br><br>
+                <label> Mail To </label><br>
+                <input type="email" name="email" placeholder="Email"> <br><br>
+                <label> Message </label><br>
+                <textarea type="text" name="message"></textarea><br><br>
+                <button type="submit"> Send Email</button>
+            </form>
+        </fieldset>
+    </div>
     </body>
 
 </html>
